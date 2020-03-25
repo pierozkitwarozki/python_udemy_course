c54de0574092b21808e1df11d6475148511bd9c2
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-03-25 13:38:33 +0100

    E-mail script uptaded with html file, but still doesn't work.

--- Scripting/em_1.py
+++ Scripting/em_1.py
@@ -2,16 +2,20 @@
 
 import smtplib  # Simple Mail Transfer Protocol
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('./HTML_Mail/index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Bartlomiej Pierog'
 email['to'] = 'bartekhenry14@gmail.com'
 email['subject'] = 'Hello from Python Script'
-
-email.set_content('I\'m a Python master ')
+email.set_content(html.substitute(name='Bart'))
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
     smtp.login("jordandan2323@gmail.com", "grazyna2017")
     smtp.send(email)
+
+# Code is good, but it doesn't work beacuse of google security problems.
