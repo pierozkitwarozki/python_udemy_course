c04405c1b876025a2e73fed1bb90adf70d0c1836
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-03-28 18:31:22 +0100

    Added sms sending script with twilio

---
+++ Scripting/SMS_Sending/sms_1.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+account_sid = 'AC02f52799703bdd2241230398c73fc8bd'
+auth_token = 'eec12fb213b177c4637ed3d9d1ede42b'
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    from_='+16626440158',
+    body='This fucking works!!!',
+    to='+48697584302'
+)
+
+print(message.sid)
